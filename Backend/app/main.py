29f3007126ccf0bd86791e44381db514012a0f28
Broski3 <arpit.bhayana13@gmail.com>
2026-07-08 01:52:20 +0530

    Add centralized application configuration

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from app.api.health import router 
-app = FastAPI()
+from app.core.config import APP_NAME,APP_VERSION
+app = FastAPI(title=APP_NAME,
+    version=APP_VERSION
+)
+    
 
 app.include_router(router)

f2b103a881fb77c0dab7137259fa226e21069159
Broski3 <arpit.bhayana13@gmail.com>
2026-07-18 16:55:00 +0530

    Implement JWT authentication and login flow

--- Backend/app/services/auth_service.py
+++ Backend/app/services/auth_service.py
@@ -3,6 +3,7 @@ from app.repository.user_repository import (find_user_by_email,save_user)
 from app.utils.security import hash_password
 from app.repository.user_repository import find_user_by_email
 from app.utils.security import verify_password
+from app.utils.security import create_access_token
 
 def register_user(user: UserRegister):
     existing_user = find_user_by_email(user.email)
@@ -22,18 +23,30 @@ def register_user(user: UserRegister):
     }
 def login_user(login_data):
     user = find_user_by_email(login_data.email)
+
     if not user:
-      return  {
-            "message":"Invalid Email or Password"
+        return {
+            "message": "Invalid Email or Password"
         }
+
     is_valid = verify_password(
         login_data.password,
         user.password
     )
+
     if is_valid:
-       return {
-          "message":"Login Successful"
-       }
-    return{
-        "message":"Invalid Email or Password"
+        access_token = create_access_token(
+            {
+                "sub": user.email
+            }
+        )
+
+        return {
+            "message": "Login Successful",
+            "access_token": access_token,
+            "token_type": "bearer"
+        }
+
+    return {
+        "message": "Invalid Email or Password"
     }
--- Backend/app/utils/security.py
+++ Backend/app/utils/security.py
@@ -1,9 +1,33 @@
 from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetotime, timedelta
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
     deprecated="auto"
 )
+SECRET_KEY = "AVI@123#"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES=30
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datatime.utcnow() + timedelta(
+        minutes = ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({
+        "exp":expire
+    })
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
+
 
 def hash_password(password: str):
     return pwd_context.hash(password)
